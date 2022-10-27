97b1a55fe1fa6b994d2f7eaaac148e5dc8ae9280
Marko <marko.dzomba.info@gmail.com>
2022-10-27 21:59:03 +0200

    navbar

--- about.js
+++ about.js
@@ -1,18 +1,18 @@
-//--------------CHANGE NAV BG COLOR ON SCROLL--------------------
-let myNav1 = document.getElementById('header');
+let myNav1 = document.getElementById("header");
 let myNav2 = document.getElementById("menu-list1");
 
-
 window.onscroll = function () {
+
     if (window.scrollY > 20) {
+
         myNav1.classList.add("scrolled")
         myNav2.classList.add("scrolled")
     } else {
         myNav1.classList.remove("scrolled")
         myNav2.classList.remove("scrolled")
     }
-}
 
+}
 
 
 
